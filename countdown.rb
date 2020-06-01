39f1d3e222fb32a3c81f8672ad014bd6ed6a1796
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-01 20:30:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
+def countdown(integer)
+  
+  while integer > 0
+    puts "#{integer} second(s)!" 
+    integer -= 1 
+  end
+  
+  return "Happy New Year!"
 
-def countdown
-end
